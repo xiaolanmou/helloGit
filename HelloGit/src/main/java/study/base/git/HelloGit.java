55334d42361491519c2cc07db86f05831870c71c
sj <1612580469@qq.com>
2022-10-13 22:50:18 +0800

    添加文件

---
+++ HelloGit/src/main/java/study/base/git/HelloGit.java
@@ -0,0 +1,13 @@
+package study.base.git;
+
+/**
+ * @Description:TODO
+ * @Author: 16125
+ * @Date:2022/10/13 22:43
+ * @Modified By:
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
